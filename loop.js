0ae95b2a4733941a9201232fea246b2f795a2adb
Sadmanjoy <93051400+Sadmanjoy@users.noreply.github.com>
2022-03-09 21:39:06 +0600

    added html and js file for messege

---
+++ loop.js
@@ -0,0 +1,21 @@
+var y=prompt("Do you love me? 1 for yes, 0 for no :")
+var x;
+
+
+
+for(var x=1; x<=500; x=x+1){
+    if(y==1)
+    document.write("<h1> I love you 2 </h1>");
+    else if(y==0)
+    document.write("<h1>OH NO</h2>");
+    else
+    document.write("");
+
+    
+}  
+for(var x=1; x<=2; x=x+1){
+    
+
+}
+
+
